0b7106a19fe4b9673fd4fc1cc44c72c23e233955
Rose Geisler <96674233+rosegeisler@users.noreply.github.com>
2026-04-03 17:31:06 -0700

    Update updateBookings.sql

--- CRUD_Querries/updateBookings.sql
+++ CRUD_Querries/updateBookings.sql
@@ -1,19 +1,2 @@
--- Checking In/Out guests
-DO $$
-DECLARE
-    bid INT;
-BEGIN
-    -- Pick a reserved booking
-    SELECT bookingid INTO bid FROM Booking
-    WHERE status = 'Reserved' AND checkedin = FALSE LIMIT 1;
-
-    RAISE NOTICE 'Using bookingid: %', bid;
-
-    -- Check in
-    UPDATE Booking SET checkedin = TRUE, status = 'Paid'
-    WHERE bookingid = bid AND checkedin = FALSE;
-
-    -- Check out
-    UPDATE Booking SET checkedout = TRUE
-    WHERE bookingid = bid AND checkedin = TRUE AND checkedout = FALSE;
-END $$;
+UPDATE Booking SET checkedin = TRUE
+WHERE bookingid =  37031;
